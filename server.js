5e0fbe70b3057991ae01016b34e482924ccc5df2
juniorr7b <juniorrivera028@gmail.com>
2026-02-03 23:53:14 -0500

    1

--- server.js
+++ server.js
@@ -11,6 +11,18 @@ const { errorHandler, notFound, requestLogger } = require('./middleware/errorHan
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
 // ==================== Mcd backendIDDLEWARES ====================
 
 // Seguridad HTTP
